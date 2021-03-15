41f0dcb5eb4c980160171f040ce9ebbcb2e0de46
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-15 20:44:51 +0000

    added functionality for failed get impressions call - tests pass

--- DAL.Instagram/Repositories/FacebookInstaImpressionsRepository.cs
+++ DAL.Instagram/Repositories/FacebookInstaImpressionsRepository.cs
@@ -22,21 +22,29 @@ namespace DAL.Instagram.Repositories
         
         public OperationResult<IEnumerable<InstaImpression>> GetImpressions(string instaId)
         {
-            var impressions = _facebookContext.Get($"{instaId}/insights", new RequestInsightParams
+            try
             {
-                metric="impressions",
-                period="day",
-                since=1607650400,
-                until=1610150400
-            });
-
-            var impressionsObj = JsonConvert.DeserializeObject<DataArray<Metric>>(impressions);
+                var impressions = _facebookContext.Get($"{instaId}/insights", new RequestInsightParams
+                {
+                    metric = "impressions",
+                    period = "day",
+                    since = 1607650400,
+                    until = 1610150400
+                });
+                
+                var impressionsObj = JsonConvert.DeserializeObject<DataArray<Metric>>(impressions);
             
-            new PostCondition().Evaluate(impressionsObj!=null);
+                new PostCondition().Evaluate(impressionsObj!=null);
 
-            return new OperationResult<IEnumerable<InstaImpression>>(
-                impressionsObj.Data.First().Insights.Select(x => new InstaImpression(DateTime.Parse(x.Time), x.Value)),
-                OperationResultEnum.Success);
+                return new OperationResult<IEnumerable<InstaImpression>>(
+                    impressionsObj.Data.First().Insights.Select(x => new InstaImpression(DateTime.Parse(x.Time), x.Value)),
+                    OperationResultEnum.Success);
+            }
+            catch (Exception)
+            {
+                return new OperationResult<IEnumerable<InstaImpression>>(Enumerable.Empty<InstaImpression>(),
+                    OperationResultEnum.Failed);
+            }
         }
     }
 }
---
+++ Tests.Unit.DAL/InstaImpressionsRepository/GetImpressionsTests/When_Facebook_Graph_Error_Occurs.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using BLL.Models.Insights;
+using Bootstrapping.Services;
+using Bootstrapping.Services.Enum;
+using Facebook;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+
+namespace Tests.Unit.DAL.InstaImpressionsRepository.GetImpressionsTests
+{
+    public class When_Facebook_Graph_Error_Occurs : When_Get_Impressions_Was_Called
+    {
+        private OperationResult<IEnumerable<InstaImpression>> _result;
+
+        protected override void When()
+        {
+            MockFacebookClient
+                .Get(Arg.Any<string>(), Arg.Any<object>())
+                .Throws<FacebookOAuthException>();
+
+            base.When();
+
+            _result = Sut.GetImpressions(TestId);
+        }
+
+        [Test]
+        public void Then_The_Response_Is_Empty()
+        {
+            Assert.IsEmpty(_result.Value);
+        }
+        
+        [Test]
+        public void Then_The_Status_Is_Fail()
+        {
+            Assert.AreEqual(OperationResultEnum.Failed,_result.Status);
+        }
+    }
+}
