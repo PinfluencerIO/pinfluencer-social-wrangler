283a2891b07fbe06354b54be483da0d62d71d6db
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-15 10:30:34 +0000

    added tests for when no users are returned from get all insta users

---
+++ Tests.Unit.BLL/InstagramFetcher/InstaUserService/ConstructedSuccessfullyTests/When_No_Users_Are_Returned.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using BLL.Models.InstaUser;
+using Bootstrapping.Services;
+using NUnit.Framework;
+using Tests.Unit.BLL.InstagramFetcher.InstaUserService.ConstructedSuccessfullyTests.Shared;
+
+namespace Tests.Unit.BLL.InstagramFetcher.InstaUserService.ConstructedSuccessfullyTests
+{
+    public class When_No_Users_Are_Returned : When_Constructed_Successfully
+    {
+        private OperationResult<InstaUserIdentityCollection> _result;
+
+        protected override void When()
+        {
+            InstaUserCollection = Enumerable.Empty<InstaUser>();
+
+            base.When();
+
+            _result = Sut.GetAll();
+        }
+        
+        [Test]
+        public void Then_Insta_User_Array_Is_Empty_Valid()
+        {
+            Assert.IsEmpty(_result.Value.InstaUserIdentities);
+        }
+
+        [Test]
+        public void Then_Has_Multiple_Was_False()
+        {
+            Assert.AreEqual(false,_result.Value.HasMultiple);
+        }
+        
+        [Test]
+        public void Then_Is_Empty_Was_True()
+        {
+            Assert.AreEqual(true,_result.Value.IsEmpty);
+        }
+    }
+}
