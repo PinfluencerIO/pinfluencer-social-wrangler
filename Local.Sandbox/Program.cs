579c9a58c350af9cb89e6c4f7d9747b5e6d088f2
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-15 10:21:31 +0000

    added sandbox project

---
+++ Local.Sandbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using DAL.UserManagement.Dtos;
+using Newtonsoft.Json;
+
+namespace Local.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var auth0User = new Auth0User
+            {
+                Id = "1232134",
+                Identities = new[]
+                {
+                    new Identity
+                    {
+                        Token =
+                            "4mXxJZ"
+                    }
+                }
+            };
+            
+            Console.WriteLine(JsonConvert.SerializeObject(auth0User));
+
+            Console.Read();
+        }
+    }
+}
