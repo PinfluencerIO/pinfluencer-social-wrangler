2a4e98d375128d5e2cca696a763ff831e5006557
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-16 23:10:57 +0000

    added auth0 middleware tests for when failed to fetch access token - tests fail

---
+++ Tests.Unit.API/Auth0Middlware/When_Fetch_Token_Error_Occurs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Auth0.AuthenticationApi.Models;
+using Auth0.Core.Exceptions;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Tests.Unit.API.Auth0Middlware.Shared;
+
+namespace Tests.Unit.API.Auth0Middlware
+{
+    public class When_Fetch_Token_Error_Occurs : When_Error_Occurs
+    {
+        protected override void When()
+        {
+            AddDefaultConfiguration();
+            SetConfiguration();
+            
+            MockAuthenticationConnection
+                .SendAsync<AccessTokenResponse>(
+                    Arg.Any<HttpMethod>(),
+                    Arg.Any<Uri>(),
+                    Arg.Any<object>(),
+                    Arg.Any<IDictionary<string, string>>()
+                )
+                .Throws<ErrorApiException>();
+            
+            base.When();
+        }
+    }
+}
