a599d8f5dc4031b41a853896efddd55b142510fd
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-17 10:45:45 +0000

    added simple authentication middleware

---
+++ API.InstaFetcher/Middleware/SimpleAuthenticationMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Bootstrapping.Services.Enum;
+using Bootstrapping.Services.Factories;
+using Bootstrapping.Services.Repositories;
+using DAL.Instagram;
+using DAL.Instagram.Dtos;
+using Facebook;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+namespace API.InstaFetcher.Middleware
+{
+    public class SimpleAuthenticationMiddleware
+    {
+        private RequestDelegate _next;
+
+        public SimpleAuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(
+            HttpContext context,
+            [FromServices] IConfiguration configuration
+        )
+        {
+            StringValues header;
+            var isHeaderPresent = context.Request.Headers.TryGetValue("InstaServiceKey",out header);
+            if (!isHeaderPresent)
+            {
+                await HandleError(context, "no 'InstaServiceKey' value was present in the request header");
+            }
+
+            var key = configuration.GetValue<string>("SimpleAuthKey");
+
+            if (header.ToString().Equals(key))
+            {
+                await _next.Invoke(context);
+            }
+            await HandleError(context, "'InstaServiceKey' value was not valid");
+        }
+
+        private static async Task HandleError(HttpContext context, string message)
+        {
+            context
+                .Response
+                .StatusCode = 401;
+            context
+                .Response
+                .ContentType = "application/json";
+            await context
+                .Response
+                .WriteAsync(JsonConvert.SerializeObject(new { error = "authorization error", message }));
+        }
+    }
+}
