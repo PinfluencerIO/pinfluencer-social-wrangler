da15ce761a35f1c04f114862e4a369ad3523c716
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-17 11:58:38 +0000

    added simple authentication middleware tests for when keys dont match - tests pass

--- Tests.Unit.API/SimpleAuthenticationMiddlware/When_Keys_Dont_Match.cs
+++ Tests.Unit.API/SimpleAuthenticationMiddlware/When_Keys_Dont_Match.cs
@@ -1,7 +1,20 @@
-﻿namespace Tests.Unit.API.SimpleAuthenticationMiddlware
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Tests.Unit.API.SimpleAuthenticationMiddlware.Shared;
+
+namespace Tests.Unit.API.SimpleAuthenticationMiddlware
 {
-    public class When_Keys_Dont_Match
+    public class When_Keys_Dont_Match : When_Error_Occurs
     {
-        
+        protected override void When()
+        {
+            var headerParams = new Dictionary<string, StringValues>();
+            headerParams.Add("InstaServiceKey","TestKey");
+            HeaderDictionary = new HeaderDictionary(headerParams);
+            ApiKeyFromConfig = "TestKey1";
+
+            base.When();
+        }
     }
 }
