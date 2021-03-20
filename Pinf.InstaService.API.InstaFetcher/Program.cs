8f4a689be67f4604c49447d34598ce5a81ebef91
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-20 02:37:53 +0000

    tweak config settings - added config.json for AWS EBS, as it overrides the default appsettings.json

--- Pinf.InstaService.API.InstaFetcher/Program.cs
+++ Pinf.InstaService.API.InstaFetcher/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Pinf.InstaService.API.InstaFetcher
@@ -13,6 +14,7 @@ namespace Pinf.InstaService.API.InstaFetcher
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => config.AddJsonFile("config.json",true,true))
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
         }
     }
