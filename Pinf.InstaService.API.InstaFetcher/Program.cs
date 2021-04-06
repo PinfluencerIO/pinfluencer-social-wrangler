b41f681020a02c3781c1ec4f396f07ca470fe459
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-04-06 15:55:08 +0100

    modified web config - removed config.json

--- Pinf.InstaService.API.InstaFetcher/Program.cs
+++ Pinf.InstaService.API.InstaFetcher/Program.cs
@@ -6,16 +6,10 @@ namespace Pinf.InstaService.API.InstaFetcher
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(config => config.AddJsonFile("config.json",true,true))
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-        }
+        public static void Main(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .Build()
+                .Run();
     }
 }
