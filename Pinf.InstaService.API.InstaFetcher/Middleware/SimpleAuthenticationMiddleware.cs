981178ed115f7f8f5f39aad20107e2dccd8ae61c
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-04-10 15:42:29 +0100

    amended equality check in auth middlware - ignore whitespace

--- Pinf.InstaService.API.InstaFetcher/Middleware/SimpleAuthenticationMiddleware.cs
+++ Pinf.InstaService.API.InstaFetcher/Middleware/SimpleAuthenticationMiddleware.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
+using static System.String;
 
 namespace Pinf.InstaService.API.InstaFetcher.Middleware
 {
@@ -26,10 +28,14 @@ namespace Pinf.InstaService.API.InstaFetcher.Middleware
             var isHeaderPresent = context.Request.Headers.TryGetValue("Simple-Auth-Key", out header);
             if (!isHeaderPresent)
                 await HandleError(context, "no 'Simple-Auth-Key' value was present in the request header");
-
+            
             var key = configuration["Simple-Auth-Key"];
-
-            await context.Response.WriteAsync($"BREAK {key} BREAK {header.ToString()} BREAK");
+            
+            var normalizedHeader = Regex.Replace(header.ToString(), @"\s", "");
+            var normalizedKey = Regex.Replace(key, @"\s", "");
+            
+            if (normalizedHeader.Equals(normalizedKey)) await _next.Invoke(context);
+            await HandleError(context, "'Simple-Auth-Key' value was not valid");
         }
 
         private static async Task HandleError(HttpContext context, string message)
