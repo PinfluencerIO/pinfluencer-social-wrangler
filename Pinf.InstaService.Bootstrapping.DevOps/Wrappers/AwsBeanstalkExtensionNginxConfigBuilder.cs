c715e0e2a01d3058d2754c94625cfd296a28eb97
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-04-22 00:16:32 +0100

    altered dev ops deploy - amended spacing in nginx file

--- Pinf.InstaService.Bootstrapping.DevOps/Wrappers/AwsBeanstalkExtensionNginxConfigBuilder.cs
+++ Pinf.InstaService.Bootstrapping.DevOps/Wrappers/AwsBeanstalkExtensionNginxConfigBuilder.cs
@@ -18,7 +18,7 @@ namespace Pinf.InstaService.Bootstrapping.DevOps.Wrappers
         {
             using var fs = File.AppendText($"{_path}{Path.DirectorySeparatorChar}{AwsBeanstalkConstants.NginxExtensionFile}");
             var firstLine = $"location {config.Url} "+"{"+Environment.NewLine;
-            var secondLine = $"\tproxy_pass\thttp://127.0.0.1:{config.Port}/;{Environment.NewLine}";
+            var secondLine = $"    proxy_pass  http://127.0.0.1:{config.Port}/;{Environment.NewLine}";
             var thirdLine = "}";
             fs.Write($"{firstLine}{secondLine}{thirdLine}");
             return this;
@@ -28,7 +28,7 @@ namespace Pinf.InstaService.Bootstrapping.DevOps.Wrappers
         {
             using var fs = File.AppendText($"{_path}{Path.DirectorySeparatorChar}{AwsBeanstalkConstants.NginxExtensionFile}");
             var firstLine = $"location {config.Url} "+"{"+Environment.NewLine;
-            var secondLine = $"\treturn {config.Status} '{config.Text}';{Environment.NewLine}";
+            var secondLine = $"    return {config.Status} '{config.Text}';{Environment.NewLine}";
             var thirdLine = "}"+Environment.NewLine;
             fs.Write($"{firstLine}{secondLine}{thirdLine}");
             return this;
