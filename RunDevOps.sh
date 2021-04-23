a47ba49de39cb30255c0589334fd25fd69e7a3c2
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-04-23 22:32:12 +0100

    amended dev ops system - added dev ops solution to isolate systems

--- RunDevOps.sh
+++
@@ -1,5 +0,0 @@
-﻿#!/bin/bash
-
-dotnet publish Pinf.InstaService.API.InstaFetcher -o deploy -c Release;
-dotnet publish Pinf.InstaService.Bootstrapping.DevOps -o DevOps;
-dotnet exec DevOps/DevOps.dll $1;
