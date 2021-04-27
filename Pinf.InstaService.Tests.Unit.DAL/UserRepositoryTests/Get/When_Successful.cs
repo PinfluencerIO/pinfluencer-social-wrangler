3b2394aa149c988087189e587e67c4d5aa6e1a94
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-04-27 19:30:39 +0100

    added tests for get profile from bubble - test for success - tests fail

--- Pinf.InstaService.Tests.Unit.DAL/UserRepositoryTests/Get/When_Successful.cs
+++ Pinf.InstaService.Tests.Unit.DAL/UserRepositoryTests/Get/When_Successful.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net;
 using NSubstitute;
 using NUnit.Framework;
+using Pinf.InstaService.Core;
 using Pinf.InstaService.Core.Dtos;
 using Pinf.InstaService.Core.Enum;
+using Pinf.InstaService.Core.Models.User;
 using Pinf.InstaService.DAL.UserManagement.Dtos;
 using Pinf.InstaService.DAL.UserManagement.Dtos.Bubble;
 
@@ -10,9 +13,15 @@ namespace Pinf.InstaService.Tests.Unit.DAL.UserRepositoryTests.Get
 {
     public class When_Successful : Given_A_UserRepository
     {
+        private OperationResult<User> _result;
+
         protected override void When( )
         {
-            Sut.Get( "1234" );
+            MockBubbleClient
+                .Get<TypeResponse<Profile>>( Arg.Any<string>( ) )
+                .Returns( ( HttpStatusCode.OK, new TypeResponse<Profile>
+                    { Type = new Profile { Id = "1234", Name = "ExampleInfluencer" } } ) );
+            _result = Sut.Get( "1234" );
         }
 
         [ Test ]
@@ -29,6 +38,18 @@ namespace Pinf.InstaService.Tests.Unit.DAL.UserRepositoryTests.Get
             MockBubbleClient
                 .Received( 1 )
                 .Get<TypeResponse<Profile>>( Arg.Is<string>( uri => uri == "profile/1234" ) );
+        }
+
+        [ Test ]
+        public void Then_Valid_User_Is_Be_Returned( )
+        {
+            Assert.True( _result.Value.Id == "1234" && _result.Value.Name == "ExampleInfluencer" );
+        }
+        
+        [ Test ]
+        public void Then_Success_Is_Returned( )
+        {
+            Assert.AreEqual( OperationResultEnum.Success, _result.Status );
         }
     }
 }
