33830582283abfcd5bd6c1f20b033a4ae54198a3
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-04-27 20:17:36 +0100

    added implementation for get profile from bubble - test for fail - tests fail

---
+++ Pinf.InstaService.Tests.Unit.DAL/UserRepositoryTests/Get/Shared/When_Called.cs
@@ -0,0 +1,25 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Pinf.InstaService.DAL.UserManagement.Dtos.Bubble;
+
+namespace Pinf.InstaService.Tests.Unit.DAL.UserRepositoryTests.Get.Shared
+{
+    public abstract class When_Called : Given_A_UserRepository
+    {
+        [ Test ]
+        public void Then_Profile_Will_Be_Verified_Once( )
+        {
+            MockBubbleClient
+                .Received( 1 )
+                .Get<TypeResponse<Profile>>( Arg.Any<string>( ) );
+        }
+
+        [ Test ]
+        public void Then_Correct_Profile_Endpoint_Is_Reached( )
+        {
+            MockBubbleClient
+                .Received( 1 )
+                .Get<TypeResponse<Profile>>( Arg.Is<string>( uri => uri == "profile/1234" ) );
+        }
+    }
+}
--- Pinf.InstaService.Tests.Unit.DAL/UserRepositoryTests/Get/When_Error_Occurs.cs
+++ Pinf.InstaService.Tests.Unit.DAL/UserRepositoryTests/Get/When_Error_Occurs.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using NSubstitute;
 using NUnit.Framework;
 using Pinf.InstaService.Core;
-using Pinf.InstaService.Core.Dtos;
 using Pinf.InstaService.Core.Enum;
 using Pinf.InstaService.Core.Models.User;
-using Pinf.InstaService.DAL.UserManagement.Dtos;
 using Pinf.InstaService.DAL.UserManagement.Dtos.Bubble;
 using Pinf.InstaService.Tests.Unit.DAL.UserRepositoryTests.Get.Shared;
 
 namespace Pinf.InstaService.Tests.Unit.DAL.UserRepositoryTests.Get
 {
-    public class When_Successful : When_Called
+    public class When_Error_Occurs : When_Called
     {
         private OperationResult<User> _result;
 
@@ -20,21 +17,20 @@ namespace Pinf.InstaService.Tests.Unit.DAL.UserRepositoryTests.Get
         {
             MockBubbleClient
                 .Get<TypeResponse<Profile>>( Arg.Any<string>( ) )
-                .Returns( ( HttpStatusCode.OK, new TypeResponse<Profile>
-                    { Type = new Profile { Id = "1234", Name = "ExampleInfluencer" } } ) );
+                .Returns( ( HttpStatusCode.NotFound, new TypeResponse<Profile> { Type = new Profile() } ) );
             _result = Sut.Get( "1234" );
         }
 
         [ Test ]
         public void Then_Valid_User_Is_Be_Returned( )
         {
-            Assert.True( _result.Value.Id == "1234" && _result.Value.Name == "ExampleInfluencer" );
+            Assert.True( _result.Value.Id == null && _result.Value.Name == null );
         }
         
         [ Test ]
         public void Then_Success_Is_Returned( )
         {
-            Assert.AreEqual( OperationResultEnum.Success, _result.Status );
+            Assert.AreEqual( OperationResultEnum.Failed, _result.Status );
         }
     }
 }
