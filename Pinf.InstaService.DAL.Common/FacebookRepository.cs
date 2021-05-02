206fad3e0d10ffe8664b87fe944d61bc9dbd9592
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-02 19:49:27 +0100

    added base facebook repository

--- Pinf.InstaService.DAL.Common/FacebookRepository.cs
+++ Pinf.InstaService.DAL.Common/FacebookRepository.cs
@@ -1,7 +1,33 @@
-﻿namespace Pinf.InstaService.DAL.Common
+﻿using System;
+using Facebook;
+using Pinf.InstaService.Crosscutting.Utils;
+
+namespace Pinf.InstaService.DAL.Common
 {
     public abstract class FacebookRepository
     {
+        protected readonly ILoggerAdapter<FacebookRepository> Logger;
+
+        protected FacebookRepository( ILoggerAdapter<FacebookRepository> logger ) { Logger = logger; }
         
+        protected( string, bool ) ValidateFacebookCall( Func<string> facebookCall )
+        {
+            var errorReturn = ( default( string ), false );
+            try { return( facebookCall( ), true ); }
+            catch( FacebookApiLimitException )
+            {
+                Logger.LogError( "facebook api limit error occured" );
+            }
+            catch( FacebookOAuthException )
+            {
+                Logger.LogError( "facebook oauth error occured" );
+            }
+            catch( FacebookApiException e )
+            {
+                Logger.LogError( "facebook api error occured" );
+                Logger.LogError( e.Message );
+            }
+            return errorReturn;;
+        }
     }
 }
