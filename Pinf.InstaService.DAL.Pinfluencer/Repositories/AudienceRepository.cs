8eb1eb0b9946be59e98859dc50c44cc10c95c33c
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-04 21:32:50 +0100

    stubbed out DAL.Pinfluencer.AudienceRepository

---
+++ Pinf.InstaService.DAL.Pinfluencer/Repositories/AudienceRepository.cs
@@ -0,0 +1,24 @@
+﻿using Pinf.InstaService.Core.Enum;
+using Pinf.InstaService.Core.Interfaces.Clients;
+using Pinf.InstaService.Core.Interfaces.Repositories;
+using Pinf.InstaService.Core.Models;
+
+namespace Pinf.InstaService.DAL.Pinfluencer.Repositories
+{
+    public class AudienceRepository : IAudienceRepository
+    {
+        private readonly IBubbleClient _bubbleClient;
+
+        public AudienceRepository( IBubbleClient bubbleClient ) { _bubbleClient = bubbleClient; }
+        
+        public OperationResultEnum Create( Audience audience )
+        {
+            return OperationResultEnum.Failed;
+        }
+
+        public OperationResultEnum Update( Audience audience )
+        {
+            throw new System.NotImplementedException( );
+        }
+    }
+}
