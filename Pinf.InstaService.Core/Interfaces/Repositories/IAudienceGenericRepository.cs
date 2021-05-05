d9fd4f198df7fd349b17191a33ef311f7defadd7
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-05 10:43:48 +0100

    changed generic repository format

--- Pinf.InstaService.Core/Interfaces/Repositories/IAudienceGenericRepository.cs
+++ Pinf.InstaService.Core/Interfaces/Repositories/IAudienceGenericRepository.cs
@@ -8,6 +8,5 @@ namespace Pinf.InstaService.Core.Interfaces.Repositories
         OperationResult<IEnumerable<T>> GetAll( string audienceId );
 
         OperationResultEnum Create( T audience );
-        
     }
 }
