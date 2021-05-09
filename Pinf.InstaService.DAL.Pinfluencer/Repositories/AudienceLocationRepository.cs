5e1868f76e9412b46c953ff601e2bebf05bca5e3
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-09 20:08:50 +0100

    added tests for DAL.AudienceLocationRepository.Create - when called - tests pass

--- Pinf.InstaService.DAL.Pinfluencer/Repositories/AudienceLocationRepository.cs
+++ Pinf.InstaService.DAL.Pinfluencer/Repositories/AudienceLocationRepository.cs
@@ -4,16 +4,30 @@ using Pinf.InstaService.Core;
 using Pinf.InstaService.Core.Enum;
 using Pinf.InstaService.Core.Interfaces.Repositories;
 using Pinf.InstaService.Core.Models.Insights;
+using Pinf.InstaService.DAL.Core.Interfaces.Handlers;
+using Pinf.InstaService.DAL.Pinfluencer.Dtos.Bubble;
 
 namespace Pinf.InstaService.DAL.Pinfluencer.Repositories
 {
     public class AudienceLocationRepository : IAudienceLocationRepository
     {
+        private readonly IBubbleDataHandler<AudienceLocationRepository> _bubbleDataHandler;
+
+        public AudienceLocationRepository( IBubbleDataHandler<AudienceLocationRepository> bubbleDataHandler )
+        {
+            _bubbleDataHandler = bubbleDataHandler;
+        }
+        
         public OperationResult<IEnumerable<AudiencePercentage<LocationProperty>>> GetAll( string audienceId ) =>
             new OperationResult<IEnumerable<AudiencePercentage<LocationProperty>>>(
                 Enumerable.Empty<AudiencePercentage<LocationProperty>>( ), OperationResultEnum.Failed );
 
         public OperationResultEnum Create( AudiencePercentage<LocationProperty> audience ) =>
             OperationResultEnum.Failed;
+
+        public AudienceLocation ModelMap( AudiencePercentage<LocationProperty> audienceLocation )
+        {
+            return new AudienceLocation( );
+        }
     }
 }
---
+++ Pinf.InstaService.Tests.Unit.DAL/AudienceLocationRepositoryTests/Given_An_AudienceLocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pinf.InstaService.Crosscutting.NUnit.PinfluencerExtensions;
+using Pinf.InstaService.DAL.Pinfluencer.Repositories;
+
+namespace Pinf.InstaService.Tests.Unit.DAL.AudienceLocationRepositoryTests
+{
+    public class Given_An_AudienceLocationRepository : DataGivenWhenThen<AudienceLocationRepository>
+    {
+        protected override void Given( )
+        {
+            base.Given( );
+            Sut = new AudienceLocationRepository( MockBubbleDataHandler );
+        }
+    }
+}
