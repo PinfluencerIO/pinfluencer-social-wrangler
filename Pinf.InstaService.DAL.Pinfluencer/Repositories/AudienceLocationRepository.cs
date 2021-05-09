ff6fd0e9b0e675e9497d77c0542f6aba113c0f7b
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-09 19:24:57 +0100

    added class for DAL.AudienceLocationRepository

---
+++ Pinf.InstaService.DAL.Pinfluencer/Repositories/AudienceLocationRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Pinf.InstaService.Core;
+using Pinf.InstaService.Core.Enum;
+using Pinf.InstaService.Core.Interfaces.Repositories;
+using Pinf.InstaService.Core.Models.Insights;
+
+namespace Pinf.InstaService.DAL.Pinfluencer.Repositories
+{
+    public class AudienceLocationRepository : IAudienceLocationRepository
+    {
+        public OperationResult<IEnumerable<AudiencePercentage<LocationProperty>>> GetAll( string audienceId ) =>
+            new OperationResult<IEnumerable<AudiencePercentage<LocationProperty>>>(
+                Enumerable.Empty<AudiencePercentage<LocationProperty>>( ), OperationResultEnum.Failed );
+
+        public OperationResultEnum Create( AudiencePercentage<LocationProperty> audience ) =>
+            OperationResultEnum.Failed;
+    }
+}
