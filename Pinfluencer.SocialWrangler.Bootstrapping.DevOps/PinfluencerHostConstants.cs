9ac75d61d401637fc372d1a8d2d9064a84d0029e
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-14 00:00:09 +0100

    modified nginx name [ nodeploy ]

--- Pinfluencer.SocialWrangler.Bootstrapping.DevOps/PinfluencerHostConstants.cs
+++ Pinfluencer.SocialWrangler.Bootstrapping.DevOps/PinfluencerHostConstants.cs
@@ -3,7 +3,7 @@
     public class PinfluencerHostConstants
     {
         public static readonly int ReverseProxyPort = 5100;
-        public static readonly string ApiUrl = "/instagram-service/";
+        public static readonly string ApiUrl = "/social-wrangler/";
         public static readonly string ProcessNamespace = "Pinfluencer.SocialWrangler.API";
         public static readonly string CertUrl = "/.well-known/pki-validation/BC074CC58368F01C6FB8CF426F41E3D1.txt";
     }
