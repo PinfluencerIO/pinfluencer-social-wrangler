865e65433cba56b6bbf77e52be63c76aff0db97e
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-05-30 17:14:23 +0100

    added implementation for DAL.InstagramAudienceGenderAgeRepository.MapMany - tests pass

--- Pinfluencer.SocialWrangler.DAL.Facebook/Repositories/InstagramAudienceGenderAgeRepository.cs
+++ Pinfluencer.SocialWrangler.DAL.Facebook/Repositories/InstagramAudienceGenderAgeRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 using Pinfluencer.SocialWrangler.Core;
 using Pinfluencer.SocialWrangler.Core.Enum;
 using Pinfluencer.SocialWrangler.Core.Models;
@@ -24,7 +26,41 @@ namespace Pinfluencer.SocialWrangler.DAL.Facebook.Repositories
 
         public override IEnumerable<AudienceCount<GenderAgeProperty>> MapMany( DataArray<Metric<object>> dtoCollection )
         {
-            return Enumerable.Empty<AudienceCount<GenderAgeProperty>>( );
+            var genderAge =
+                dtoCollection.Data.First( ).Insights.First( ).Value as IEnumerable<KeyValuePair<string, JToken>>;
+            var outputResult =
+                genderAge.Select( x =>
+                {
+                    var genderString = x.Key.Split( "." )[ 0 ];
+                    int ageMin;
+                    int? ageMax;
+                    if( x.Key.Split( "." )[ 1 ].Contains( "+" ) )
+                    {
+                        ageMin = int.Parse( x.Key.Split( "." )[ 1 ].Split( "+" )[ 0 ] );
+                        ageMax = null;
+                    }
+                    else
+                    {
+                        ageMin = int.Parse( x.Key.Split( "." )[ 1 ].Split( "-" )[ 0 ] );
+                        ageMax = int.Parse( x.Key.Split( "." )[ 1 ].Split( "-" )[ 1 ] );
+                    }
+                    return new AudienceCount<GenderAgeProperty>
+                    {
+                        Count = ( int ) x.Value,
+                        Property = new GenderAgeProperty
+                        {
+                            Gender = genderString switch
+                            {
+                                "F" => GenderEnum.Female,
+                                "M" => GenderEnum.Male,
+                                "U" => GenderEnum.Unknown,
+                                _ => GenderEnum.Unknown
+                            },
+                            AgeRange = new Tuple<int, int?>( ageMin, ageMax )
+                        }
+                    };
+                } );
+            return outputResult;
         }
     }
 }
---
+++ Pinfluencer.SocialWrangler.Tests.Unit.DAL/InstagramAudienceGenderAgeRepositoryTests/MapMany/When_Called.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Pinfluencer.SocialWrangler.Core.Enum;
+using Pinfluencer.SocialWrangler.Crosscutting.Utils;
+using Pinfluencer.SocialWrangler.DAL.Facebook.Dtos;
+
+namespace Pinfluencer.SocialWrangler.Tests.Unit.DAL.InstagramAudienceGenderAgeRepositoryTests.MapMany
+{
+    public class When_Called : Given_An_InstagramGenderAgeRepository
+    {
+        [ Test ]
+        public void Then_Mapping_Is_Correct( )
+        {
+            var result = SUT.MapMany( new DataArray<Metric<object>>
+            {
+                Data = new [ ]
+                {
+                    new Metric<object>
+                    {
+                        Insights = new [ ]
+                        {
+                            new Insight<object>
+                            {
+                                Time = "2021-05-30T07:00:00+0000",
+                                Value = JsonConvert.DeserializeObject( "{\"F.18-24\": 36,\"F.25-34\": 4,\"F.45-54\": 1,\"M.18-24\": 75,\"M.25-34\": 9,\"M.35-44\": 1,\"M.45-54\": 2,\"M.55-64\": 1,\"M.65+\": 1,\"U.18-24\": 7,\"U.25-34\": 1}" )
+                            }
+                        }
+                    }
+                }
+            } );
+            CollectionAssert.AreEquivalent( new (GenderEnum Male, int, int?, int)[]
+            {
+                ( GenderEnum.Female, 18, 24, 36 ),
+                ( GenderEnum.Female, 25, 34, 4 ),
+                ( GenderEnum.Female, 45, 54, 1 ),
+                ( GenderEnum.Male, 18, 24, 75 ),
+                ( GenderEnum.Male, 25, 34, 9 ),
+                ( GenderEnum.Male, 35, 44, 1 ),
+                ( GenderEnum.Male, 45, 54, 2 ),
+                ( GenderEnum.Male, 55, 64, 1 ),
+                ( GenderEnum.Male, 65, null, 1 ),
+                ( GenderEnum.Unknown, 18, 24, 7 ),
+                ( GenderEnum.Unknown, 25, 34, 1 )
+            }, result
+                .Select( x => ( x.Property.Gender,
+                    x.Property.AgeRange.Item1,
+                    x.Property.AgeRange.Item2,
+                    x.Count ) ));
+        }
+    }
+}
