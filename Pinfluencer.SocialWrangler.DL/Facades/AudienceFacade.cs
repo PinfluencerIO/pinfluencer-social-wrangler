cfb0099971c3d10f2a151c275a1326fd3a21518b
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-06-09 15:30:03 +0100

    added tests for DL.AudienceFacade.GetFromSocial - added implementation for sad paths - tests pass

--- Pinfluencer.SocialWrangler.DL/Facades/AudienceFacade.cs
+++ Pinfluencer.SocialWrangler.DL/Facades/AudienceFacade.cs
@@ -1,6 +1,8 @@
-﻿using Pinfluencer.SocialWrangler.Core;
+﻿using System.Linq;
+using Pinfluencer.SocialWrangler.Core;
 using Pinfluencer.SocialWrangler.Core.Enum;
 using Pinfluencer.SocialWrangler.Core.Models;
+using Pinfluencer.SocialWrangler.Core.Models.Insights;
 using Pinfluencer.SocialWrangler.DL.Core.Interfaces.Contract;
 
 namespace Pinfluencer.SocialWrangler.DL.Facades
@@ -22,33 +24,62 @@ namespace Pinfluencer.SocialWrangler.DL.Facades
         
         public ObjectResult<Audience> GetFromSocial( )
         {
-            var user = _socialInsightUserFacade
-                .GetFirstUser( )
-                .Value;
+            var userResult = _socialInsightUserFacade
+                .GetFirstUser( );
+            if( userResult.Status == OperationResultEnum.Failed )
+            {
+                return new ObjectResult<Audience>
+                {
+                    Status = OperationResultEnum.Failed,
+                    Value = null
+                };
+            }
+
+            var user = userResult.Value;
+
+            var audienceAgeResult = _socialAudienceFacade
+                .GetAudienceAgeInsights( user.Id );
+
+            var audienceGenderResult = _socialAudienceFacade
+                .GetAudienceGenderInsights( user.Id );
+
+            var audienceCountryResult = _socialAudienceFacade
+                .GetAudienceCountryInsights( user.Id );
+
+            var engagementRateResult = _socialContentFacade
+                .GetEngagementRate( );
+
+            var impressionsResult = _socialContentFacade
+                .GetImpressions( user.Id );
+
+            var reachResult = _socialContentFacade
+                .GetReach( user.Id );
+
+            var warning = audienceAgeResult.Status == OperationResultEnum.Failed ||
+                           audienceGenderResult.Status == OperationResultEnum.Failed ||
+                           audienceCountryResult.Status == OperationResultEnum.Failed ||
+                           engagementRateResult.Status == OperationResultEnum.Failed ||
+                           impressionsResult.Status == OperationResultEnum.Failed ||
+                           reachResult.Status == OperationResultEnum.Failed;
+
             return new ObjectResult<Audience>
             {
-                Status = OperationResultEnum.Success,
+                Status = warning ? OperationResultEnum.Warning : OperationResultEnum.Success,
                 Value = new Audience
                 {
-                    AudienceAge = _socialAudienceFacade
-                        .GetAudienceAgeInsights( user.Id )
-                        .Value,
-                    AudienceGender = _socialAudienceFacade
-                        .GetAudienceGenderInsights( user.Id )
-                        .Value,
-                    AudienceCountry = _socialAudienceFacade
-                        .GetAudienceCountryInsights( user.Id )
-                        .Value,
-                    EngagementRate = _socialContentFacade
-                        .GetEngagementRate( )
-                        .Value,
+                    AudienceAge = audienceAgeResult.Status == OperationResultEnum.Success ?
+                        audienceAgeResult.Value : Enumerable.Empty<AudiencePercentage<AgeProperty>>( ),
+                    AudienceGender = audienceGenderResult.Status == OperationResultEnum.Success ?
+                        audienceGenderResult.Value : Enumerable.Empty<AudiencePercentage<GenderEnum>>( ),
+                    AudienceCountry = audienceCountryResult.Status == OperationResultEnum.Success ?
+                        audienceCountryResult.Value : Enumerable.Empty<AudiencePercentage<CountryProperty>>( ),
+                    EngagementRate = engagementRateResult.Status == OperationResultEnum.Success ?
+                        engagementRateResult.Value : default,
                     Followers = user.Followers,
-                    Impressions = _socialContentFacade
-                        .GetImpressions( user.Id )
-                        .Value,
-                    Reach = _socialContentFacade
-                        .GetReach( user.Id )
-                        .Value
+                    Impressions = impressionsResult.Status == OperationResultEnum.Success ?
+                        impressionsResult.Value : default,
+                    Reach = reachResult.Status == OperationResultEnum.Success ?
+                        reachResult.Value : default,
                 }
             };
         }
